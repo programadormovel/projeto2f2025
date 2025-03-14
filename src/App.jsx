23d5ac09de129b716900449d2831d4069b896370
programadormovel <programadormovel@gmail.com>
2025-03-14 09:44:29 -0300

    santos meu amor

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+class App extends Component{
+
+  render(){
+
+    return (
+      <div>
+        <h3>Aula React Vite</h3>
+        <p>Retomada de Conteúdo</p>
+      </div>
+    )
+  }
+}
+
+export default App
